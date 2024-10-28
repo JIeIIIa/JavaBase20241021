a6c445e7fef564f773e01ed1e7502eeb26991643
Oleksii Onishcenko <onishchenko.oleksii@gmail.com>
2024-10-28 20:46:55 +0200

    Lecture-03 - Add limes to show merge conflicts

--- src/com/gmail/onishchenko/lectures/lecture03/GitDemo.java
+++ src/com/gmail/onishchenko/lectures/lecture03/GitDemo.java
@@ -3,5 +3,6 @@ package com.gmail.onishchenko.lectures.lecture03;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Some problem");
     }
 }
