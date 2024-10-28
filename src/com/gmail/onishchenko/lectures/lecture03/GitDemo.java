fff3f6471d3c6feeb724ef39ce43a677db2f03ea
Oleksii Onishcenko <onishchenko.oleksii@gmail.com>
2024-10-28 21:10:21 +0200

    Lecture-03 - Add problems

--- src/com/gmail/onishchenko/lectures/lecture03/GitDemo.java
+++ src/com/gmail/onishchenko/lectures/lecture03/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("My name is Oleksii");
+        System.out.println("Add more problem");
     }
 }
