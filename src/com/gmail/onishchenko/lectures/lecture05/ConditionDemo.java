f3de11a140c66f0268932cb53a4c3916cf80e979
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-11-04 20:48:46 +0200

    Lecture-05 - Conditions: part-2

--- src/com/gmail/onishchenko/lectures/lecture05/ConditionDemo.java
+++ src/com/gmail/onishchenko/lectures/lecture05/ConditionDemo.java
@@ -2,15 +2,30 @@ package com.gmail.onishchenko.lectures.lecture05;
 
 public class ConditionDemo {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 10;
+        int a = 11;
+        int b = 11;
 
-        if (a % 2 == 0) {
-            if (b % 2 == 0) {
-                System.out.println("00");
-            } else {
-                System.out.println("Unexpected");
-            }
+        System.out.println(" AND ");
+        if ((a % 2 == 0) & (b % 2 == 0)) {
+            System.out.println("00");
+        } else {
+            System.out.println("Unexpected");
         }
+
+        System.out.println(" OR ");
+        if ((a % 2 == 0) | (b % 2 == 0)) {
+            System.out.println("00 or 10 or 01");
+        } else {
+            System.out.println("Unexpected");
+        }
+        System.out.println((a % 2 == 0) | (b % 2 == 0) ? "00 or 10 or 01" : "Unexpected");
+
+        System.out.println(" Negotiation");
+        if (!((a % 2 == 0) | (b % 2 == 0))) {
+            System.out.println("00 or 10 or 01");
+        } else {
+            System.out.println("Unexpected");
+        }
+
     }
 }
