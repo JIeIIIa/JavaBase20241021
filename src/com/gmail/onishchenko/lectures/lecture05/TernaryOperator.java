e474d31dad5dc7c659ba16e320280ff74289e522
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-11-04 20:27:13 +0200

    Lecture-05 - Ternary operator: problem

---
+++ src/com/gmail/onishchenko/lectures/lecture05/TernaryOperator.java
@@ -0,0 +1,17 @@
+package com.gmail.onishchenko.lectures.lecture05;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int result;
+        int a = 10;
+        int b = 25;
+
+        if (a > b) {
+           result = 1;
+        } else {
+            result = -1;
+        }
+
+        System.out.println(result);
+    }
+}
