9818a74600fa72ea97e3f36d0fd5024f5c7c1cbe
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-11-04 21:36:25 +0200

    Lecture-05 - Scanner demo

---
+++ src/com/gmail/onishchenko/lectures/lecture05/ScannerDemo.java
@@ -0,0 +1,18 @@
+package com.gmail.onishchenko.lectures.lecture05;
+
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        com.gmail.onishchenko.lectures.lecture05.subpackage.Scanner myScanner = new com.gmail.onishchenko.lectures.lecture05.subpackage.Scanner();
+        System.out.println("Enter a value (0-9): ");
+        int value = scanner.nextInt();
+        System.out.println("Entered value == " + value);
+        scanner.nextLine();
+        System.out.println("Enter a string value: ");
+        String line = scanner.nextLine();
+        System.out.printf("Line == '%s'\n", line);
+    }
+}
