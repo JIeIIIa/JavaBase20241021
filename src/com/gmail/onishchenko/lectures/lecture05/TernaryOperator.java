015ede92ff149677029a07147485312bf88f3235
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-11-04 20:33:50 +0200

    Lecture-05 - Inner ternary operator

--- src/com/gmail/onishchenko/lectures/lecture05/TernaryOperator.java
+++ src/com/gmail/onishchenko/lectures/lecture05/TernaryOperator.java
@@ -3,10 +3,10 @@ package com.gmail.onishchenko.lectures.lecture05;
 public class TernaryOperator {
     public static void main(String[] args) {
         int result;
-        int a = 10;
+        int a = 40;
         int b = 25;
 
-        result = a > b ? 1 : -1;
+        result = a > b ? a % 2 == 0 ? 123 : -123 : -1;
 
 
         System.out.println(result);
