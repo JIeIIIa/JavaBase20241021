bcac01323720bbd8dddc315ec791c0fe21b20650
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-11-11 19:54:47 +0200

    Homework-5.1 - TaxCalculator (improve code)

--- src/com/gmail/onishchenko/homework/TaxCalculator.java
+++ src/com/gmail/onishchenko/homework/TaxCalculator.java
@@ -8,15 +8,16 @@ public class TaxCalculator {
         System.out.print("Input value: ");
         double value = scanner.nextDouble();
 
+        double taxRate;
         if (value <= 10_000) {
-            double tax = value * 2.5 / 100;
-            System.out.println("Your calculated tax is " + tax);
+            taxRate = 2.5;
         } else if (value <= 25_000) {
-            double tax = value * 4.3 / 100;
-            System.out.println("Your calculated tax is " + tax);
+            taxRate = 4.3;
         } else {
-            double tax = value * 6.7 / 100;
-            System.out.println("Your calculated tax is " + tax);
+            taxRate = 6.7;
         }
+
+        double tax = value * taxRate / 100;
+        System.out.println("Your calculated tax is " + tax);
     }
 }
