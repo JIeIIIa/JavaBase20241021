680524b4941c47feed77cc98195650252a167117
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-11-11 19:43:15 +0200

    Homework-4.1 - Sales (approach with methods)

--- src/com/gmail/onishchenko/homework/Sales.java
+++ src/com/gmail/onishchenko/homework/Sales.java
@@ -5,17 +5,18 @@ public class Sales {
         int number = 1;
         int days = 5;
         double totalPrice = 12153.41;
-        double averagePrice = totalPrice / days;
-        System.out.printf("Product No %d: smartphone,\n", number);
-        System.out.printf("total sales for %d days is EUR %.2f,\n", days, totalPrice);
-        System.out.printf("sales by day is EUR %.2f.\n", averagePrice);
+        summary(totalPrice, days, number);
 
         number++;
         days = 7;
         totalPrice = 10486.85;
-        averagePrice = totalPrice / days;
+        summary(totalPrice, days, number);
+    }
+
+    private static void summary(double totalPrice, int days, int number) {
+        double averagePrice = Math.round(totalPrice / days * 100) / 100.0;
         System.out.printf("Product No %d: smartphone,\n", number);
         System.out.printf("total sales for %d days is EUR %.2f,\n", days, totalPrice);
-        System.out.printf("sales by day is EUR %.2f.\n", averagePrice);
+        System.out.println("sales by day is EUR " + averagePrice);
     }
 }
