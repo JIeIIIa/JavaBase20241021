add48e35b94b60e54e654fc1d77bc4e6f4c5f8ff
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-11-14 19:48:01 +0200

    Homework-6.1 - Summarize (loops) - methods approach

--- src/com/gmail/onishchenko/homework/Summarize.java
+++ src/com/gmail/onishchenko/homework/Summarize.java
@@ -9,19 +9,30 @@ public class Summarize {
         int bound = scanner.nextInt();
 //        int bound = 6;
 
-        System.out.println(" -= FOR LOOP =-");
+        forLoop(bound);
+        whileLoop(bound);
+        doWhileLoop(bound);
+    }
+
+    private static void doWhileLoop(int bound) {
+        System.out.println("\n\n\n");
+        System.out.println(" -= DO-WHILE LOOP =-");
+        int i = 1;
         int sum = 0;
-        for (int i = 1; i <= bound ; i++) {
+        do {
             sum += i;
             System.out.printf("%d) Num is %d, sum is %d\n", i, i, sum);
-        }
+            i++;
+        } while (i <= bound);
         System.out.println("------------------------");
         System.out.printf("Sum of numbers is %d", sum);
+    }
 
+    private static void whileLoop(int bound) {
         System.out.println("\n\n\n");
         System.out.println(" -= WHILE LOOP =-");
         int i = 1;
-        sum = 0;
+        int sum = 0;
         while (i <= bound) {
             sum += i;
             System.out.printf("%d) Num is %d, sum is %d\n", i, i, sum);
@@ -29,16 +40,15 @@ public class Summarize {
         }
         System.out.println("------------------------");
         System.out.printf("Sum of numbers is %d", sum);
+    }
 
-        System.out.println("\n\n\n");
-        System.out.println(" -= DO-WHILE LOOP =-");
-        i = 1;
-        sum = 0;
-        do {
+    public static void forLoop(int bound) {
+        System.out.println(" -= FOR LOOP =-");
+        int sum = 0;
+        for (int i = 1; i <= bound; i++) {
             sum += i;
             System.out.printf("%d) Num is %d, sum is %d\n", i, i, sum);
-            i++;
-        } while (i <= bound);
+        }
         System.out.println("------------------------");
         System.out.printf("Sum of numbers is %d", sum);
     }
