1d52486f2c283b64470dde11aaff6605bd67674f
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-11-18 21:33:32 +0200

    Lecture-9 - Multi dimension array (part-2)

--- src/com/gmail/onishchenko/lectures/lecture09/MultiDimensionArrays.java
+++ src/com/gmail/onishchenko/lectures/lecture09/MultiDimensionArrays.java
@@ -2,12 +2,32 @@ package com.gmail.onishchenko.lectures.lecture09;
 
 public class MultiDimensionArrays {
     public static void main(String[] args) {
-        int[][] matrix = new int[4][7];
+        int[][] matrix = new int[][]{
+                {1, 2, 3, 4},
+                {5, 6, 7, 8}
+        };
+        matrix[1][3] = 42;
+//        matrix[1][13] = -13; // error!
 
         for (int i = 0; i < matrix.length; i++) {
-            int[] row = matrix[i];
-            print(row);
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
         }
+
+        System.out.println("--------  CUBE  ---------");
+        int[][][] cube = new int[5][3][4];
+        for (int i = 0; i < cube.length; i++) {
+            System.out.println("Layer #" + i);
+            for (int j = 0; j < cube[i].length; j++) {
+                for (int k = 0; k < cube[i][j].length; k++) {
+                    System.out.print(cube[i][j][k] + "\t");
+                }
+                System.out.println();
+            }
+        }
+        System.out.println("------  END CUBE  --------");
     }
 
     public static void print(int[] array) {
