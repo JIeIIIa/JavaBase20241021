04622e567ac2111dbecd5ef3c5ca7b855488c00d
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-11-25 21:25:43 +0200

    Lecture-11 - Wrappers (part-2)

--- src/com/gmail/onishchenko/lectures/lecture11/WrapperDemo.java
+++ src/com/gmail/onishchenko/lectures/lecture11/WrapperDemo.java
@@ -5,12 +5,28 @@ import java.util.List;
 
 public class WrapperDemo {
     public static void main(String[] args) {
-        Character character = Character.valueOf('c');
-        Integer i = Integer.valueOf(42);
+//        Character character = Character.valueOf('c');
+        oldStyle();
+        autoBoxingUnboxing();
+    }
 
+    private static void oldStyle() {
+        Integer i = new Integer(42);
+
+        Integer result = new Integer(i.intValue() + 10);
+        System.out.println("result == " + result);
         ArrayList list = new ArrayList();
         list.add(i);
         System.out.println("First object of list is " + list.get(0));
+    }
 
+    private static void autoBoxingUnboxing() {
+        Integer i = null;
+
+        Integer result = i + 10;
+        System.out.println("result == " + result);
+        ArrayList list = new ArrayList();
+        list.add(i);
+        System.out.println("First object of list is " + list.get(0));
     }
 }
