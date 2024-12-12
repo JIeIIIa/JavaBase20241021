9ebf50f393ae7bc0884033174aa58ca5d7af38eb
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-12-12 20:06:51 +0200

    Lecture-13 - Abstract class (part-3)

---
+++ src/com/gmail/onishchenko/lectures/lecture13/pizza/VegetarianPizza.java
@@ -0,0 +1,7 @@
+package com.gmail.onishchenko.lectures.lecture13.pizza;
+
+public abstract class VegetarianPizza extends Pizza {
+    public VegetarianPizza(String name) {
+        super(name);
+    }
+}
