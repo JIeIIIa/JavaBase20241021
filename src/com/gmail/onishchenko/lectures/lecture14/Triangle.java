7725356d2d25aac4e99bb6c1e4d2b66053fa3e18
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-12-16 19:55:07 +0200

    Lecture-14 - Static nested class (part-2)

--- src/com/gmail/onishchenko/lectures/lecture14/Triangle.java
+++ src/com/gmail/onishchenko/lectures/lecture14/Triangle.java
@@ -3,6 +3,7 @@ package com.gmail.onishchenko.lectures.lecture14;
 import java.util.Arrays;
 
 public class Triangle {
+    public static final int EDGE_COUNT = 3;
     private Edge[] edges;
 
     public Triangle(Edge[] edges) {
@@ -17,12 +18,22 @@ public class Triangle {
     }
 
     public static class Edge {
+        private static int createdInstances = 0;
         private int length;
         private String color;
 
         public Edge(int length, String color) {
+            createdInstances++;
             this.length = length;
             this.color = color;
+        }
+
+        public static int getCreatedInstances() {
+            return createdInstances;
+        }
+
+        public static void outerField() {
+            System.out.println("Triangle must have " + EDGE_COUNT + " sides");
         }
 
         @Override
--- src/com/gmail/onishchenko/lectures/lecture14/TriangleRunner.java
+++ src/com/gmail/onishchenko/lectures/lecture14/TriangleRunner.java
@@ -1,16 +1,26 @@
 package com.gmail.onishchenko.lectures.lecture14;
 
+import static com.gmail.onishchenko.lectures.lecture14.Triangle.EDGE_COUNT;
+
 public class TriangleRunner {
     public static void main(String[] args) {
+        System.out.println("Created edges: " + Triangle.Edge.getCreatedInstances());
+        int createdEdges = 0;
         int[] sides = {3, 4, 5};
+        Triangle.Edge blueEdge = new Triangle.Edge(5, "blue");
         Triangle triangle = new Triangle(new Triangle.Edge[]{
                 new Triangle.Edge(3, "black"),
                 new Triangle.Edge(4, "green"),
-                new Triangle.Edge(5, "blue")
+                blueEdge
         });
+        createdEdges += 3;
         System.out.println(triangle);
 
         sides[0] = -123456;
         System.out.println(triangle);
+        System.out.println("Created edges: " + Triangle.Edge.getCreatedInstances());
+
+        Triangle.Edge.outerField();
+        System.out.println("Triangle must have " + EDGE_COUNT + " sides");
     }
 }
