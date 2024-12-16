5cf8962feb72d529ed06a755d5c8ecdceec9a3f1
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-12-16 19:41:13 +0200

    Lecture-14 - Static nested class

--- src/com/gmail/onishchenko/lectures/lecture14/Triangle.java
+++ src/com/gmail/onishchenko/lectures/lecture14/Triangle.java
@@ -3,16 +3,34 @@ package com.gmail.onishchenko.lectures.lecture14;
 import java.util.Arrays;
 
 public class Triangle {
-    private int[] sides;
+    private Edge[] edges;
 
-    public Triangle(int[] sides) {
-        this.sides = Arrays.copyOf(sides, sides.length);
+    public Triangle(Edge[] edges) {
+        this.edges = Arrays.copyOf(edges, edges.length);
     }
 
     @Override
     public String toString() {
         return "Triangle{" +
-                "sides=" + Arrays.toString(sides) +
+                "edges=" + Arrays.toString(edges) +
                 '}';
+    }
+
+    public static class Edge {
+        private int length;
+        private String color;
+
+        public Edge(int length, String color) {
+            this.length = length;
+            this.color = color;
+        }
+
+        @Override
+        public String toString() {
+            return "Edge{" +
+                    "length=" + length +
+                    ", color='" + color + '\'' +
+                    '}';
+        }
     }
 }
--- src/com/gmail/onishchenko/lectures/lecture14/TriangleRunner.java
+++ src/com/gmail/onishchenko/lectures/lecture14/TriangleRunner.java
@@ -3,7 +3,11 @@ package com.gmail.onishchenko.lectures.lecture14;
 public class TriangleRunner {
     public static void main(String[] args) {
         int[] sides = {3, 4, 5};
-        Triangle triangle = new Triangle(sides);
+        Triangle triangle = new Triangle(new Triangle.Edge[]{
+                new Triangle.Edge(3, "black"),
+                new Triangle.Edge(4, "green"),
+                new Triangle.Edge(5, "blue")
+        });
         System.out.println(triangle);
 
         sides[0] = -123456;
