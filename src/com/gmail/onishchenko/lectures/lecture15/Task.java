7796a73b711a9dc94c89ea4143470d48a3f7e0d5
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-12-19 21:42:25 +0200

    Lecture-15 - Move status to upper level

--- src/com/gmail/onishchenko/lectures/lecture15/Task.java
+++ src/com/gmail/onishchenko/lectures/lecture15/Task.java
@@ -35,20 +35,4 @@ public class Task {
                 '}';
     }
 
-    public static class Status {
-        public static final Status TODO = new Status("TODO");
-        public static final Status IN_PROGRESS = new Status("IN_PROGRESS");
-        public static final Status DONE = new Status("DONE");
-
-        private String name;
-
-        private Status(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return name;
-        }
-    }
 }
--- src/com/gmail/onishchenko/lectures/lecture15/TaskDemo.java
+++ src/com/gmail/onishchenko/lectures/lecture15/TaskDemo.java
@@ -2,7 +2,7 @@ package com.gmail.onishchenko.lectures.lecture15;
 
 public class TaskDemo {
     public static void main(String[] args) {
-        Task task = new Task(Task.Status.TODO, "learn Java");
+        Task task = new Task(Status.TODO, "learn Java");
         System.out.println("Primary task is: " + task);
 //        task.setStatus(new Task.Status("FAKE STATUS")); // error! constructor is private
 //        System.out.println("Changed status is " + task.getStatus());
