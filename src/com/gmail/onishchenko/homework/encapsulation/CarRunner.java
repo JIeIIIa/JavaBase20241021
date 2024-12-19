030b4bbb1d612cae41af591fc7a84a9ed8dc3355
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-12-19 20:40:59 +0200

    Homework-12.1 - Car (part-2)

---
+++ src/com/gmail/onishchenko/homework/encapsulation/CarRunner.java
@@ -0,0 +1,8 @@
+package com.gmail.onishchenko.homework.encapsulation;
+
+public class CarRunner {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.start();
+    }
+}
