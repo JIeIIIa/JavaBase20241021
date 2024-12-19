ae5897bc30d693d22bbb856aeb184fcc26d10b90
Onishchenko Oleksii <onishchenko.oleksii@gmail.com>
2024-12-19 22:01:12 +0200

    Lecture-15 - Enum (part-3)

--- src/com/gmail/onishchenko/lectures/lecture15/Status.java
+++ src/com/gmail/onishchenko/lectures/lecture15/Status.java
@@ -1,9 +1,16 @@
 package com.gmail.onishchenko.lectures.lecture15;
 
-public enum Status {
-    TODO(7),
+public enum Status implements Runnable {
+    TODO(7) ,
     IN_PROGRESS(42),
-    DONE(1_000_000);
+    DONE(1_000_000) {
+        private static final String MESSAGE = "ALL DONE)";
+
+        @Override
+        public void run() {
+            System.out.println(MESSAGE);
+        }
+    };
 
     Status(int defaultDuration) {
         this.defaultDuration = defaultDuration;
@@ -13,5 +20,10 @@ public enum Status {
 
     public int getDefaultDuration() {
         return defaultDuration;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Running...");
     }
 }
--- src/com/gmail/onishchenko/lectures/lecture15/TaskDemo.java
+++ src/com/gmail/onishchenko/lectures/lecture15/TaskDemo.java
@@ -5,8 +5,20 @@ public class TaskDemo {
         Task task = new Task(Status.TODO, "learn Java");
         System.out.println("Primary task is: " + task);
         System.out.println("Expected duration: " + task.getStatus().getDefaultDuration());
+
+        System.out.println(" -= IN PROGRESS =-");
         task.setStatus(Status.IN_PROGRESS);
-        System.out.println("Expected duration after status was changed: " + task.getStatus().getDefaultDuration());
+        Status currentStatus = task.getStatus();
+        System.out.println("Current status is " + currentStatus);
+        System.out.println("Expected duration after status was changed: " + currentStatus.getDefaultDuration());
+        currentStatus.run();
+
+        System.out.println(" -= DONE =-");
+        task.setStatus(Status.DONE);
+        currentStatus = task.getStatus();
+        System.out.println("Current status is " + currentStatus);
+        System.out.println("Expected duration after status was changed: " + currentStatus.getDefaultDuration());
+        currentStatus.run();
 //        task.setStatus(new Task.Status("FAKE STATUS")); // error! constructor is private
 //        System.out.println("Changed status is " + task.getStatus());
     }
